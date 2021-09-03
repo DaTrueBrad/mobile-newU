c922896cfb4a9d80db46d26168c0311cf40162dd
DaTrueBrad <bottbrady@gmail.com>
2021-09-03 12:38:03 -0600

    added JS functionality to manipulate form data

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
-function test(e) {
+let formEl = document.getElementById('first-form')
+let formGoal = document.getElementById('goal-dropdown')
+let formGoalLength = document.getElementById('length-dropdown')
+
+
+formEl.onsubmit = async (e) => {
     e.preventDefault()
-    console.log('formData')
+
+    let data = new FormData(e.target)
+    let value = Object.fromEntries(data.entries())
+
+    console.log(value)
 }
