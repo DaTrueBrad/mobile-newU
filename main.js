24b07d6df5ce2faa8931a68e51f2c5ac119aa94b
DaTrueBrad <bottbrady@gmail.com>
2021-09-07 10:23:25 -0600

    fixed forms

--- main.js
+++ main.js
@@ -7,7 +7,28 @@ formEl.onsubmit = async (e) => {
     e.preventDefault()
 
     let data = new FormData(e.target)
-    let value = Object.fromEntries(data.entries())
+    // let value = Object.fromEntries(data.entries())
 
-    console.log(value)
+    let { goal, goalLength, frequency, length, lowerBack, upperBack, shoulder, neck, elbow, wrist, hip, knee, ankle, foot, finger, other } = Object.fromEntries(data.entries())
+
+    let bodyObj = {
+        goal,
+        goalLength,
+        frequency,
+        length,
+        lowerBack,
+        upperBack,
+        shoulder,
+        neck,
+        elbow,
+        wrist,
+        hip,
+        knee,
+        ankle,
+        foot,
+        finger,
+        other
+    }
+    console.log(bodyObj)
+    // console.log(value)
 }
